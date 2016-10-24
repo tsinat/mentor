b8b456522cb6e4bbd36a0c0d986182101cf2767c
Tsinat Zeree <cheru87@gmail.com>
2016-10-24 00:42:32 -0700

    generate token

--- server/router.js
+++ server/router.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { signup } from './controllers/authentication';
-import User from './models/user';
 
 export default function(app) {
-    app.get('/signup', signup());
+    app.post('/signup', signup);
+
 }
